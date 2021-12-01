a01d4d49a6408171b9c73534383ecacde3093861
Davide De Simone <desimonedavide97@gmail.com>
2021-12-01 22:29:30 +0100

    modify the prepare.py script, add the processed dataset to dvc and push to the remote storage

--- src/prepare.py
+++ src/prepare.py
@@ -1,16 +1,34 @@
 import numpy as np
 import pandas as pd
 import glob
+import os
+import ruamel.yaml as yaml
 from sklearn.model_selection import train_test_split
 
-dataset_path="C:/Users/super/OneDrive/Desktop/materiale università/Magistrale/2° anno/Software Engineering for AI Systems/progetto-vep-su-github/Volcanic-eruption-prediction/data/predict-volcanic-eruptions_dataset"
-train_path= dataset_path + "/train.csv"       #csv con colonne "segment_id" e "time_to_eruption". 'Segment_id' denota un csv di training nella cartella 'train'
-sample_submission_path= dataset_path + "/sample_submission.csv" #contiene i nomi dei csv di test. Usato per costruire il test set
-train_folder_path= dataset_path + "/train"
-test_folder_path= dataset_path + "/test"
-train_files_path= train_folder_path + "/*"
-test_files_path= test_folder_path + "/*"
-processed_dataset_path = "C:/Users/super/OneDrive/Desktop/prova_processed_dataset/"
+separator = os.sep
+cur_dir= os.getcwd()  #path assoluto della directory del progetto
+dataset_path= os.path.join(cur_dir, 'data', 'raw', 'predict-volcanic-eruptions_dataset')
+train_path= os.path.join(dataset_path, 'train.csv')       #csv con colonne "segment_id" e "time_to_eruption". 'Segment_id' denota un csv di training nella cartella 'train'
+sample_submission_path= os.path.join(dataset_path, 'sample_submission.csv') #contiene i nomi dei csv di test. Usato per costruire il test set
+train_folder_path= os.path.join(dataset_path, 'train')
+test_folder_path= os.path.join(dataset_path, 'test')
+train_files_path= os.path.join(train_folder_path, '*')
+test_files_path= os.path.join(test_folder_path, '*')
+processed_dataset_path = os.path.join(cur_dir, 'data', 'processed') #non mettere il separator nel join se no il path che lo precede è transformato in C:
+processed_training_set_path = processed_dataset_path + separator +'processed_training_set.csv'
+processed_val_set_path = processed_dataset_path + separator + 'processed_validation_set.csv'
+y_train_path = processed_dataset_path + separator + 'y_train.csv'
+y_val_path = processed_dataset_path + separator + 'y_validation.csv'
+proc_test_set_path = processed_dataset_path + separator + 'processed_test_set.csv'
+params_path = os.path.join(cur_dir, 'params.yaml')
+
+# Read data preparation parameters
+with open(params_path, "r") as params_file:
+    try:
+        params = yaml.safe_load(params_file)
+        params = params["prepare"]
+    except yaml.YAMLError as exc:
+        print(exc)
 
 train = pd.read_csv(train_path)
 sample_submission = pd.read_csv(sample_submission_path)
@@ -29,7 +47,7 @@ missed_groups = list()
 missing_sensors_train = list()      #prima chiamato for_df
 
 for item in train_frags:
-    name = int(item.split('.')[-2].split('/')[-1])
+    name = int(item.split('.')[-2].split(separator)[-1]) #da path/nome.csv prende il nome (numerico) dei file di traning
     at_least_one_missed = 0
     frag = pd.read_csv(item)
     missed_group = list()
@@ -66,7 +84,7 @@ missed_groups = list()
 missing_sensors_test = list()
 
 for item in test_frags:
-    name = int(item.split('.')[-2].split('/')[-1])
+    name = int(item.split('.')[-2].split(separator)[-1])
     at_least_one_missed = 0
     frag = pd.read_csv(item)
     missed_group = list()
@@ -95,7 +113,7 @@ missing_sensors_test = pd.DataFrame(
 
 #create the training set csv
  
-def build_features(signal, ts, sensor_id):       #signal=colonna di un csv, ts= nome csv. Calcola delle statistiche per ogni colonna di ogni csv, cioè per le rilevazioni di ogni sensore
+def build_features(signal, ts, sensor_id):       #signal=colonna di un csv, ts=nome csv. Calcola delle statistiche per ogni colonna di ogni csv, cioè per le rilevazioni di ogni sensore
     X = pd.DataFrame()
     f = np.fft.fft(signal)  #compute the Discrete Fourier Transform of a sequence. It converts a space or time signal to signal of the frequency domain
     f_real = np.real(f)
@@ -128,10 +146,10 @@ def build_features(signal, ts, sensor_id):       #signal=colonna di un csv, ts= 
 train_set = list()
 j=0
 for seg in train.segment_id:         #trasforma ogni csv(60k righe, 10 colonne) in una riga con 243 features, calcolando delle statistiche su ogni colonna del csv
-    signals = pd.read_csv(train_folder_path + f'/{seg}.csv')
+    signals = pd.read_csv(train_folder_path +separator+ f'{seg}.csv')
     train_row = []
     if j%500 == 0:
-        print(j)                   #ogni 500 csv processati
+        print('training files processed:' + f'{j}')                   #ogni 500 csv processati
     for i in range(0, 10):
         sensor_id = f'sensor_{i+1}'
         train_row.append(build_features(signals[sensor_id].fillna(0), seg, sensor_id))
@@ -145,55 +163,73 @@ train_set = train_set.reset_index() #reset index of the dataframe object to defa
 train_set = train_set.rename(columns={'index': 'segment_id'})
 train_set = pd.merge(train_set, train, on='segment_id')
 
-"""crea dataset con meno features, 75 su 243"""
-# drop_cols = list()
-# for col in train_set.columns:     #scarta le colonne con poca correlazione con la colonna target
-#     if col == 'segment_id':
-#         continue
-#     if abs(train_set[col].corr(train_set['time_to_eruption'])) < 0.01:   #corr() compute pairwise correlation(default Pearson) of columns, excluding NA/null values
-#         drop_cols.append(col)
+"""crea dataset con features più significative, 75 invece di 243"""
+drop_cols = list()
+for col in train_set.columns:     #scarta le colonne con poca correlazione con la colonna target
+    if col == 'segment_id':
+        continue
+    if abs(train_set[col].corr(train_set['time_to_eruption'])) < 0.01:   #corr() compute pairwise correlation(default Pearson) of columns, excluding NA/null values
+        drop_cols.append(col)
 
-# not_to_drop_cols = list()
+not_to_drop_cols = list()
 
-# for col1 in train_set.columns:
-    # for col2 in train_set.columns:
-    #     if col1 == col2:
-    #         continue
-    #     if col1 == 'segment_id' or col2 == 'segment_id': 
-    #         continue
-    #     if col1 == 'time_to_eruption' or col2 == 'time_to_eruption':
-    #         continue
-    #     if abs(train_set[col1].corr(train_set[col2])) > 0.98:          #scarta una tra due colonne correllate tra loro più del 98%
-    #         if col2 not in drop_cols and col1 not in not_to_drop_cols:
-    #             drop_cols.append(col2)
-    #             not_to_drop_cols.append(col1)
+for col1 in train_set.columns:
+    for col2 in train_set.columns:
+        if col1 == col2:
+            continue
+        if col1 == 'segment_id' or col2 == 'segment_id': 
+            continue
+        if col1 == 'time_to_eruption' or col2 == 'time_to_eruption':
+            continue
+        if abs(train_set[col1].corr(train_set[col2])) > 0.98:          #scarta una tra due colonne correllate tra loro più del 98%
+            if col2 not in drop_cols and col1 not in not_to_drop_cols:
+                drop_cols.append(col2)
+                not_to_drop_cols.append(col1)
 
 
 train = train_set.drop(['segment_id', 'time_to_eruption'], axis=1)
 y_train = train_set['time_to_eruption']
 
-# reduced_y = y_train.copy()
-# reduced_train = train.copy()
-# reduced_train = reduced_train.drop(drop_cols, axis=1)
+reduced_y = y_train.copy()
+reduced_train = train.copy()
+reduced_train = reduced_train.drop(drop_cols, axis=1)
 
-#splitta il trining set in train e validetion sets
-train, val, y_train, y_val = train_test_split(train, y_train, random_state=666, test_size=0.2, shuffle=True) #random_state sets a seed to the random generator, so that train-test splits are always deterministic.It's for reproducibility. If you don't set a seed, it is different each time
+#splitta il traning set in train e validation sets
+reduced_train, reduced_val, reduced_y, reduced_y_val = train_test_split(reduced_train, reduced_y, train_size=params["train_size"], test_size=params["test_size"], random_state=params["random_state"]) #random_state sets a seed to the random generator, so that train-test splits are always deterministic.It's for reproducibility. If you don't set a seed, it is different each time
 
-train.to_csv(processed_dataset_path + 'processed_training_set.csv', index=False)
-val.to_csv(processed_dataset_path + 'processed_validation_set.csv', index=False)
-y_train.to_csv(processed_dataset_path + 'y_train.csv', index=False)
-y_val.to_csv(processed_dataset_path + 'y_validation.csv', index=False)
+print("Writing file {} to disk.".format(processed_training_set_path))
+reduced_train.to_csv(processed_training_set_path, index=False)
 
+print("Writing file {} to disk.".format(processed_val_set_path))
+reduced_val.to_csv(processed_val_set_path, index=False)
 
-#reduced_train, reduced_val, reduced_y, reduced_y_val = train_test_split(reduced_train, reduced_y, random_state=666, test_size=0.2, shuffle=True)
+print("Writing file {} to disk.".format(y_train_path))
+reduced_y.to_csv(y_train_path, index=False)
+
+print("Writing file {} to disk.".format(y_val_path))
+reduced_y_val.to_csv(y_val_path, index=False)
+
+# train, val, y_train, y_val = train_test_split(train, y_train, train_size=params["train_size"], test_size=params["test_size"], random_state=params["random_state"]) #random_state sets a seed to the random generator, so that train-test splits are always deterministic.It's for reproducibility. If you don't set a seed, it is different each time
+
+# print("Writing file {} to disk.".format(processed_training_set_path))
+# train.to_csv(processed_training_set_path, index=False)
+
+# print("Writing file {} to disk.".format(processed_val_set_path))
+# val.to_csv(processed_val_set_path, index=False)
+
+# print("Writing file {} to disk.".format(y_train_path))
+# y_train.to_csv(y_train_path, index=False)
+
+# print("Writing file {} to disk.".format(y_val_path))
+# y_val.to_csv(y_val_path, index=False)
 
 test_set = list()
 j=0
 for seg in sample_submission.segment_id:
-    signals = pd.read_csv(test_folder_path + f'/{seg}.csv')
+    signals = pd.read_csv(test_folder_path +separator+ f'{seg}.csv')
     test_row = []
     if j%500 == 0:
-        print(j)
+        print('test files processed:' + f'{j}')
     for i in range(0, 10):
         sensor_id = f'sensor_{i+1}'
         test_row.append(build_features(signals[sensor_id].fillna(0), seg, sensor_id))
@@ -206,7 +242,8 @@ test_set = test_set.reset_index()
 test_set = test_set.rename(columns={'index': 'segment_id'})
 test_set = pd.merge(test_set, missing_sensors_test, on='segment_id')
 test = test_set.drop(['segment_id'], axis=1)
-test.to_csv(processed_dataset_path +'processed_test_set.csv', index=False)      #non possiamo calcolare l'errore sul test set perchè mancano i valori della variabile target dato che il codice è preso da una challenge su kaggle
+reduced_test = test.copy()
+reduced_test = reduced_test.drop(drop_cols, axis=1)
 
-# reduced_test = test.copy()
-# reduced_test = reduced_test.drop(drop_cols, axis=1)
+print("Writing file {} to disk.".format(proc_test_set_path))
+reduced_test.to_csv(proc_test_set_path, index=False)      #non possiamo calcolare l'errore sul test set perchè mancano i valori della variabile target dato che il codice è preso da una challenge su kaggle
