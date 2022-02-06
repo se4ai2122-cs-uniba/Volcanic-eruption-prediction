20a002b79c0c677599fd0d598c43176bb5eeee00
Davide De Simone <desimonedavide97@gmail.com>
2022-02-06 12:16:56 +0100

    modified dockerfile to containerize locust

--- tests/load_test/locustfile.py
+++ tests/load_test/locustfile.py
@@ -1,6 +1,6 @@
 """
 Run load tests:
-locust -f load_test/locustfile.py --host http://localhost:5000     #host= api address
+locust -f tests/load_test/locustfile.py --host http://localhost:5000     #host(api address) = http://host.docker.internal:5000 nel container
 """
 
 import random
@@ -11,8 +11,8 @@ from os.path import dirname
 
 models = ['LGBMRegressor', 'XGBRegressor', 'Neural_Network'] #i tre tipi di modello tra cui scegliere
 
-folder_path=dirname(dirname(__file__)) + "/data/raw/predict-volcanic-eruptions_dataset/test/"
-csv = os.listdir(folder_path) #inserisce nella variabile csv tutti i nomi dei csv nella cartella test
+folder_path= dirname(dirname(dirname(__file__))) + "/data/raw/predict-volcanic-eruptions_dataset/test/"   #folder_path='test.zip' nel locustfile nel container
+csv = os.listdir(folder_path)                             #inserisce nella variabile csv tutti i nomi dei csv nella cartella test.    z=zipfile.ZipFile(folder_path)  csv=z.namelist() nel container
 
 def selectRandom(valori):
   return random.choice(valori)
@@ -34,6 +34,6 @@ class VulcanicPredictionUser(HttpUser):
         file_name=selectRandom(csv)
         path_file = folder_path + file_name
         files = [
-            ('file', (path_file, open(path_file, 'rb'), 'application/vnd.ms-excel'))
+            ('file', (path_file, open(path_file, 'rb'), 'application/vnd.ms-excel'))       # z.open nel container
             ]
         self.client.post(url, files=files)
