15acf39799df440211228cdd328d73d5fd0015f7
giusegal <50910146+giusegal@users.noreply.github.com>
2022-02-12 16:01:47 +0100

    Update predict.jsx

--- frontend/src/predict.jsx
+++ frontend/src/predict.jsx
@@ -31,7 +31,6 @@ function Predict() {
             setPrediction(response.data.data)
             setFile(response.data)
             setMessage(response.data)
-            console.log(response.data)
             if((response.data.message)==("OK")){
                 setMessage(response.data.message="")}
         })
